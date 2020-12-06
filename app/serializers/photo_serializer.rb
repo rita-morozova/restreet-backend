64234b1914058f8d01738c865fbcf75ad3cd5f19
Margarita Morozova <ritamorozova@yahoo.com>
2020-12-05 22:11:35 -0800

    some changes

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,5 +1,5 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :image, :username, :user_id
+  attributes :id, :image, :username, :user_id, :created_at
 
   # has_many :likes
 end
